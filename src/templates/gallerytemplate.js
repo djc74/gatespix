8fe6510b67b7d098f4b086344da1632a57f1e49d
djc74 <davidcooper974@gmail.com>
2020-03-04 13:37:19 +0000

    svg arrows added

--- src/templates/gallerytemplate.js
+++ src/templates/gallerytemplate.js
@@ -2,28 +2,64 @@ import React from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
-import Carousel, { Dots } from "@brainhubeu/react-carousel"
+import Carousel from "@brainhubeu/react-carousel"
 import "@brainhubeu/react-carousel/lib/style.css"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageHeader from "../components/pageheader"
+// import ArrowLeft from "../images/arrow-left.svg"
+// import ArrowRight from "../images/arrow-right.svg"
 
-const EventGalleryGrid = styled.ul`
-  align-items: flex-start;
-  display: flex;
-  flex-direction: column;
-  padding: 2rem 0;
-  flex-wrap: wrap;
-`
+const ArrowLeft = () => (
+  <svg
+    width="24"
+    height="40"
+    viewBox="0 0 24 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M22.8284 33.1716C24.3905 34.7337 24.3905 37.2663 22.8284 38.8284C21.2663 40.3905 18.7337 40.3905 17.1716 38.8284L1.17157 22.8284C-0.390524 21.2663 -0.390524 18.7337 1.17157 17.1716L17.1716 1.17157C18.7337 -0.390524 21.2663 -0.390524 22.8284 1.17157C24.3905 2.73367 24.3905 5.26633 22.8284 6.82843L9.65685 20L22.8284 33.1716Z"
+      fill="#FEFEFE"
+    />
+  </svg>
+)
 
-const EventGalleryListingItem = styled.li`
-  list-style: none;
-  min-width: 70%;
-  height: 500px;
-  margin: 0 auto 2rem;
-  overflow: hidden;
-`
+const ArrowRight = () => (
+  <svg
+    width="24"
+    height="40"
+    viewBox="0 0 24 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1.17157 6.82843C-0.390524 5.26633 -0.390524 2.73367 1.17157 1.17157C2.73367 -0.390524 5.26633 -0.390524 6.82843 1.17157L22.8284 17.1716C24.3905 18.7337 24.3905 21.2663 22.8284 22.8284L6.82843 38.8284C5.26633 40.3905 2.73367 40.3905 1.17157 38.8284C-0.390524 37.2663 -0.390524 34.7337 1.17157 33.1716L14.3431 20L1.17157 6.82843Z"
+      fill="#FEFEFE"
+    />
+  </svg>
+)
+
+// const EventGalleryGrid = styled.ul`
+//   align-items: flex-start;
+//   display: flex;
+//   flex-direction: column;
+//   padding: 2rem 0;
+//   flex-wrap: wrap;
+// `
+
+// const EventGalleryListingItem = styled.li`
+//   list-style: none;
+//   min-width: 70%;
+//   height: 500px;
+//   margin: 0 auto 2rem;
+//   overflow: hidden;
+// `
 
 const EventImage = styled(Img)`
   max-height: 500px;
@@ -44,7 +80,15 @@ const GalleryTemplate = ({ data, pageContext }) => {
     <Layout>
       <SEO title="Gallery" keywords={[`Emma Gates`, `Pictures`]} />
       <PageHeader pagetitle={collection} />
-      <Carousel arrows>
+      <Carousel
+        arrowLeft={<ArrowLeft />}
+        // arrowLeftDisabled={<Icon name="angle-left" />}
+        arrowRight={<ArrowRight />}
+        // arrowRightDisabled={<Icon name="angle-right" />}
+        addArrowClickHandler
+        // arrows
+        infinite
+      >
         {data.allContentfulImage.edges.map((edge, i) => (
           <div key={i}>
             <EventImage fluid={edge.node.image.fluid} alt={edge.node.caption} />
